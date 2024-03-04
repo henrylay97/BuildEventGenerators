53bfdcd8e3fd9458f39e540304b6a71683fe9ab6
Henry Lay <h.lay@lancaster.ac.uk>
2024-03-04 03:05:01 -0600

    Update genie generator script

--- evgen_scripts/gen_genie_events.sh
+++ evgen_scripts/gen_genie_events.sh
@@ -1,6 +1,6 @@
-export BASE_DIR_GEN=/exp/sbnd/data/users/hlay/ncpizero/generators
-export SAVE_DIR=$BASE_DIR_GEN/genie
-export NEVTS=500000
+export FLUX_DIR=/exp/sbnd/data/users/hlay/ncpizero/plots/NCPiZeroAv16/flux_hists
+export SAVE_DIR=/exp/sbnd/data/users/hlay/ncpizero/generators/genie
+export NEVTS=5000000
 
 source /exp/sbnd/app/users/hlay/generators/BuildEventGenerators/setup_generators.sh
 
@@ -10,9 +10,9 @@ for flavour in "${!flavours[@]}"
 do
     export pdg=${flavours[$flavour]}
 
-    gevgen -p $pdg -n $NEVTS -t 1000180400 -f $BASE_DIR_GEN/flux/sbnd_flux.root,flux_sbnd_${flavour} -e 0,10 --cross-sections /cvmfs/larsoft.opensciencegrid.org/products/genie_xsec/v3_04_00/NULL/AR2320i00000-k250-e1000/data/gxspl-FNALsmall.xml --tune AR23_20i_00_000 -o $SAVE_DIR/genie_${flavour}.root
+    gevgen -p $pdg -n $NEVTS -t 1000180400 -f $FLUX_DIR/sbnd_flux.root,flux_sbnd_${flavour} -e 0,10 --cross-sections /cvmfs/larsoft.opensciencegrid.org/products/genie_xsec/v3_04_00/NULL/AR2320i00000-k250-e1000/data/gxspl-FNALsmall.xml --tune AR23_20i_00_000 -o $SAVE_DIR/genie_${flavour}.root --event-generator-list NC
 
-    PrepareGENIE -i $SAVE_DIR/genie_${flavour}.root -t 1000180400[1] -o $SAVE_DIR/genie_${flavour}_prep.root -f $BASE_DIR_GEN/flux/sbnd_flux.root,flux_sbnd_${flavour}
+    PrepareGENIE -i $SAVE_DIR/genie_${flavour}.root -t 1000180400[1] -o $SAVE_DIR/genie_${flavour}_prep.root -f $FLUX_DIR/sbnd_flux.root,flux_sbnd_${flavour}
 
     nuisflat -i GENIE:$SAVE_DIR/genie_${flavour}_prep.root -o $SAVE_DIR/genie_${flavour}_prep.flat.root
 done
